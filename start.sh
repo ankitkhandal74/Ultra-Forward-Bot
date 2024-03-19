1107f9026f6583df0f4fd1f126b53e4a8222fe40
Ankit khandal <160460793+ankitkhandal74@users.noreply.github.com>
2024-03-19 09:28:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/ankitkhandal74/Ultra-Forward-Bot ankitkhandal74/Ultra-Forward-Bot
+  git clone https://github.com/ankitkhandal74/Ultra-Forward-Bot /Ultra-Forward-Bot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
+  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot -b $BRANCH /main
 fi
-cd ankitkhandal74/Ultra-Forward-Bot
+cd /Ultra-Forward-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
